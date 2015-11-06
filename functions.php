d4e1fff458bdf6f2cedc93a996d5852f9c2d2ba1
csnedecor <csnedecor@worthwild.com>
2015-11-06 12:50:25 -0500

    Adds ability to change javascript, changes color on skill wheels

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_scripts', 10 );
+function child_theme_enqueue_scripts() {
+  wp_register_script( 'my_child_theme_script',
+    get_stylesheet_directory_uri() . '/js/zerif.js', array("jquery", "zerif_knob_nav"), '20120206', true);
+  wp_enqueue_script( 'my_child_theme_script' );
+}
+
 // enqueue the parent theme stylesheet with a low priority so it loads after bootstrap CSS
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 11 );
 function theme_enqueue_styles() {
