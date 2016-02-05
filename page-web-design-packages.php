924b1657fd3517a0e8cbc54b9ad1976962ea9b52
csnedecor <csnedecor@worthwild.com>
2016-02-05 12:09:01 -0500

    Adds standard and premium menus to price page

--- page-web-design-packages.php
+++ page-web-design-packages.php
@@ -6,6 +6,8 @@
 <?php
   $basic_features_column_1   = get_field('basic_features_column_1');
   $basic_features_column_2   = get_field('basic_features_column_2');
+  $standard_menu      = get_field('standard_menu');
+  $premium_menu      = get_field('premium_menu');
 ?>
 
   <!-- MAIN CONTENT
@@ -31,7 +33,7 @@
         </div><!-- /.section-header -->
 
         <?php if(!empty($basic_features_column_1)) : ?>
-          <div class="row basic-features" data-scrollreveal="enter left after 0.15s over 1s">
+          <div class="row basic-features" id="basic-features" data-scrollreveal="enter left after 0.15s over 1s">
             <h3>Basic Features</h3>
              <div class="col-md-6 basic-feat-1">
               <?php echo $basic_features_column_1; ?>
@@ -52,12 +54,22 @@
 
             <?php
               $package_price   = get_field('price');
+              $package_subtitle = get_field('package_subtitle')
             ?>
 
             <div class="col-lg-4 col-sm-4 design-package-box" data-scrollreveal="enter left after 0.15s over 1s">
 
               <!-- PACKAGE HEADING -->
-              <h3 class="package-title"><?php the_title(); ?></h3>
+              <div class="package-title">
+                <h3><?php the_title(); ?></h3>
+                <?php if(!empty($package_subtitle)) : ?>
+                  <h4><?php echo $package_subtitle ?></h4>
+                <?php endif; ?>
+              </div><!-- /.package-title -->
+              <!-- <h3 class="package-title"><?php the_title(); ?></h3>
+              <h4>
+
+              </h4> -->
               <h3 class="package-price">$<?php echo $package_price; ?></h3>
               <div class="package-content">
                 <?php the_content(); ?>
@@ -67,6 +79,24 @@
 
           <?php endwhile; wp_reset_query();?>
         </div><!-- /.row -->
+
+      <div class="row row-eq-height design-menu-wrap">
+
+        <?php if(!empty($standard_menu)) : ?>
+          <div class="col-md-6 design-menus" id="standard-menu" data-scrollreveal="enter left after 0.15s over 1s">
+            <h3>Standard Menu</h3>
+            <?php echo $standard_menu ?>
+          </div><!-- /.col-md-6 -->
+        <?php endif; ?>
+
+        <?php if(!empty($premium_menu)) : ?>
+          <div class="col-md-6 design-menus" id="premium-menu" data-scrollreveal="enter left after 0.15s over 1s">
+            <h3>Premium Menu</h3>
+            <?php echo $premium_menu ?>
+          </div><!-- /.col-md-6 -->
+        <?php endif; ?>
+
+      </div><!-- /.row row-eq-height -->
 
       </div><!-- container -->
     </section><!-- /.packages #packages-->
