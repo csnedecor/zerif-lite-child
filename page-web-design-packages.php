d24483fcffc7f27903c913638b160b9bd0f9eec8
csnedecor <csnedecor@worthwild.com>
2016-02-05 09:14:39 -0500

    Fixes some formatting issues for code readability

--- page-web-design-packages.php
+++ page-web-design-packages.php
@@ -3,6 +3,11 @@
     get_header();
 ?>
 
+<?php
+  $basic_features_column_1   = get_field('basic_features_column_1');
+  $basic_features_column_2   = get_field('basic_features_column_2');
+?>
+
   <!-- MAIN CONTENT
 ================================================== -->
 
@@ -25,50 +30,43 @@
 
         </div><!-- /.section-header -->
 
-          <?php
-            $basic_features_column_1   = get_field('basic_features_column_1');
-            $basic_features_column_2   = get_field('basic_features_column_2');
-            $standard_menu      = get_field('standard_menu');
-            $premium_menu      = get_field('premium_menu');
-          ?>
+        <?php if(!empty($basic_features_column_1)) : ?>
+          <div class="row basic-features" data-scrollreveal="enter left after 0.15s over 1s">
+            <h3>Basic Features</h3>
+             <div class="col-md-6 basic-feat-1">
+              <?php echo $basic_features_column_1; ?>
+            </div><!-- /.col-md-6 .basic-feat-1 -->
+            <div class="col-md-6">
+              <?php echo $basic_features_column_2; ?>
+            </div><!-- /.col-md-6 -->
+          </div><!-- /.row .basic-features -->
+        <?php endif; ?>
 
-          <?php if(!empty($basic_features_column_1)) : ?>
-            <div class="row basic-features" data-scrollreveal="enter left after 0.15s over 1s">
-                <h3>Basic Features</h3>
-                <div class="col-md-6 basic-feat-1">
-                  <?php echo $basic_features_column_1; ?>
-                </div><!-- /.col-md-6 .basic-feat-1 -->
-                <div class="col-md-6">
-                  <?php echo $basic_features_column_2; ?>
-                </div><!-- /.col-md-6 -->
-              </div><!-- /.row .basic-features -->
-          <?php endif; ?>
+        <div class="row row-eq-height">
 
-      <div class="row row-eq-height">
+          <!-- PACKAGE PRICING -->
+          <!-- Use Custom Post Type Design Packages for the following loop -->
+          <?php $loop = new WP_Query( array('post_type' => 'design_package', 'order_by' => 'post_id', 'order' => 'ASC') ); ?>
 
-        <!-- PACKAGE PRICING -->
-        <!-- Use Custom Post Type Design Packages for the following loop -->
-        <?php $loop = new WP_Query( array('post_type' => 'design_package', 'order_by' => 'post_id', 'order' => 'ASC') ); ?>
+          <?php while($loop->have_posts()) : $loop->the_post(); ?>
 
-        <?php while($loop->have_posts()) : $loop->the_post(); ?>
+            <?php
+              $package_price   = get_field('price');
+            ?>
 
-          <?php
-            $package_price   = get_field('price');
-          ?>
+            <div class="col-lg-4 col-sm-4 design-package-box" data-scrollreveal="enter left after 0.15s over 1s">
 
-          <div class="col-lg-4 col-sm-4 design-package-box" data-scrollreveal="enter left after 0.15s over 1s">
+              <!-- PACKAGE HEADING -->
+              <h3 class="package-title"><?php the_title(); ?></h3>
+              <h3 class="package-price">$<?php echo $package_price; ?></h3>
+              <div class="package-content">
+                <?php the_content(); ?>
+              </div>
 
-            <!-- PACKAGE HEADING -->
-            <h3 class="package-title"><?php the_title(); ?></h3>
-            <h3 class="package-price">$<?php echo $package_price; ?></h3>
-            <div class="package-content">
-              <?php the_content(); ?>
-            </div>
+            </div><!-- focus-box -->
 
-          </div><!-- focus-box -->
-
-        <?php endwhile; wp_reset_query();?>
-      </div><!-- /.row -->
+          <?php endwhile; wp_reset_query();?>
+        </div><!-- /.row -->
 
       </div><!-- container -->
     </section><!-- /.packages #packages-->
