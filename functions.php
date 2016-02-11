6ae2a195f61e8372f24150edf686abe0028cf71f
csnedecor <csnedecor@worthwild.com>
2016-02-11 10:59:01 -0500

    Adds widget to allow a custom contact form entry on front page, removes smooth scroll (was causing bugs)

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_scripts', 10 );
 function child_theme_enqueue_scripts() {
+
   wp_register_script( 'my_child_theme_script',
     get_stylesheet_directory_uri() . '/js/zerif.js', array("jquery", "zerif_knob_nav"), '20120206', true);
   wp_enqueue_script( 'my_child_theme_script' );
@@ -11,6 +12,7 @@ function child_theme_enqueue_scripts() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 11 );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_script('zerif_child', get_stylesheet_directory_uri() . '/js/zerif.js');
 }
 
 add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles', 12 );
@@ -18,6 +20,8 @@ function child_theme_enqueue_styles() {
 
   // remove child theme style sheet from being called before parent stylesheet
   wp_deregister_style( 'zerif_style' );
+    wp_dequeue_script('zerif_script');
+    wp_deregister_script('zerif_script');
 
   // enqueue child theme style sheet again
   wp_register_style( 'my_child_theme_style',
@@ -27,3 +31,23 @@ function child_theme_enqueue_styles() {
     'all' );
   wp_enqueue_style( 'my_child_theme_style' );
 }
+
+// ===================================================
+// REGISTER CUSTOM SIDEBARS
+// ===================================================
+
+add_action('widgets_init', 'zerif_child_register_widgets');
+
+// Register front page Contact Form widget
+function zerif_child_register_widgets() {
+
+    register_sidebar(
+        array(
+            'name'          => 'Contact us section widgets',
+            'id'            => 'sidebar-jetpackcontactus',
+            'before_widget' => '',
+            'after_widget'  => ''
+            )
+        );
+}
+?>
